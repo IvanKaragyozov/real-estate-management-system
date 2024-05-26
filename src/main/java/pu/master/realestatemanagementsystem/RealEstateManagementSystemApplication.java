d184001392afa91eeaceb7f4432af0bb3bff8083
IvanKaragyozov <ivankaragyozov0@gmail.com>
2024-05-26 13:32:10 +0300

    RMS: Minor refactoring

--- src/main/java/pu/master/realestatemanagementsystem/RealEstateManagementSystemApplication.java
+++ src/main/java/pu/master/realestatemanagementsystem/RealEstateManagementSystemApplication.java
@@ -1,22 +1,17 @@
 package pu.master.realestatemanagementsystem;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pu.master.realestatemanagementsystem.endpoints.AgentCLI;
 
 
 @SpringBootApplication
-public class RealEstateManagementSystemApplication {
+public class RealEstateManagementSystemApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(RealEstateManagementSystemApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(final AgentCLI agentCLI) {
-		return args -> agentCLI.start();
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RealEstateManagementSystemApplication.class, args);
+    }
 
 }
