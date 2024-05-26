03e14068ab272a8675bd803cbc9ca9d28d7f08a9
IvanKaragyozov <ivankaragyozov0@gmail.com>
2024-05-26 13:32:36 +0300

    RMS: Created dummy repository

--- src/main/java/pu/master/realestatemanagementsystem/repositories/AgentRepository.java
+++ src/main/java/pu/master/realestatemanagementsystem/repositories/AgentRepository.java
@@ -1,2 +1,31 @@
-package pu.master.realestatemanagementsystem.repositories;public class AgentRepository {
+package pu.master.realestatemanagementsystem.repositories;
+
+
+import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+import pu.master.realestatemanagementsystem.models.entities.Agent;
+
+
+@Repository
+public class AgentRepository
+{
+
+    private final Map<String, Agent> agents = new HashMap<>();
+
+    @PostConstruct
+    public void populateData()
+    {
+        agents.put("Tony", new Agent(1, "Tony"));
+        agents.put("Georgy", new Agent(2, "Georgy"));
+        agents.put("Emil", new Agent(3, "Emil"));
+        agents.put("Mariya", new Agent(4, "Mariya"));
+        agents.put("Pesho", new Agent(5, "Pesho"));
+    }
+
+    public Map<String, Agent> getAgents()
+    {
+        return agents;
+    }
 }
